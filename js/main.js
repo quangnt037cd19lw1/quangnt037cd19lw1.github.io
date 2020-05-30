cf96121e3b800cd199b42bbaff1941b9b9fbb3a2
quangnt037cd19lw1 <quangnt037cd19lw1@gmail.com>
2020-05-30 09:19:26 +0700

    ahihi

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const inputs = document.querySelectorAll('.controls input');
+// console.log(inputs);
 
-function handleUpdate() {
+function handleUpdate(event) {
   const suffix = this.dataset.sizing || '';
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+  // console.log(suffix);
+  // console.log(this.name);
+  // console.log(this.value);
+  // console.log(event);
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
